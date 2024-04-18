46b17b0d6ae61cc80b347c17fd35380d6bd2614f
Om Nigam <om2021064@akgec.ac.in>
2024-04-18 19:45:21 +0530

    completed hero home

--- src/components/HomeComponents/HeroPage1.tsx
+++ src/components/HomeComponents/HeroPage1.tsx
@@ -1,9 +1,47 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
+import { useState } from "react";
 
 const HeroPage1 = () => {
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: any) => {
+    const x = (window.innerWidth / 2 - event.pageX) * 0.05;
+    const y = (window.innerHeight / 2 - event.pageY) * 0.05;
+    setOffset({ x, y });
+  };
   return (
-    <Carousel>
+    <Carousel
+      showStatus={false}
+      showArrows={false}
+      autoPlay={true}
+      infiniteLoop={true}
+      renderIndicator={(onClickHandler, isSelected, index, label) => {
+        if (isSelected) {
+          return (
+            <li
+              className="inline-block mr-3 w-3 h-3 rounded-full bg-[linear-gradient(rgb(0,_237,_164)_0%,_rgb(106,_125,_241)_100%);]"
+              onClick={onClickHandler}
+              key={index}
+              role="button"
+              tabIndex={0}
+              title={label}
+            />
+          );
+        }
+        return (
+          <li
+            className="inline-block mr-3 w-3 h-3 rounded-full bg-transparent border border-blue-400"
+            onClick={onClickHandler}
+            key={index}
+            role="button"
+            tabIndex={0}
+            title={label}
+          />
+        );
+      }}
+    >
       <div className=" w-full h-[110vh] object-contain bg-[url('/bg3.jpg')] bg-no-repeat bg-center bg-cover text-center py-10">
         <div className="w-24 h-24 rounded-full mx-auto flex items-center justify-center bg-[linear-gradient(rgb(255,_219,_79)_0%,_rgb(255,_174,_79)_35%,_rgb(255,_42,_117)_100%)]">
           <span className="material-symbols-outlined text-white font-extrabold text-[4.5rem] ">
@@ -36,8 +74,87 @@ const HeroPage1 = () => {
           </button>
         </div>
       </div>
-      <div className=" p-20"></div>
-      <div className=" w-full h-[110vh] object-contain bg-[url('/bg1.jpg')] bg-no-repeat bg-center bg-cover py-10">
+      <div
+        className="relative w-full h-[110vh] py-10 "
+        onMouseMove={handleMouseMove}
+      >
+        <div className="absolute  w-full h-full object-contain ">
+          {/* Background Image */}
+          <div
+            style={{
+              transform: `translate(${offset.x}px, ${offset.y}px)`,
+              transition: "transform 0.1s ease-out",
+            }}
+            className="absolute left-[48%] inset-0  h-[100%] w-[50%] object-contain"
+          >
+            <Image
+              src="/shapes-bg.webp"
+              alt="Background"
+              layout="fill"
+              objectFit="contain"
+              style={{ opacity: 1 }}
+              className="w-full h-full"
+            />
+          </div>
+
+          {/* Foreground Image */}
+          <div
+            style={{
+              transform: `translate(${offset.x * 2}px, ${offset.y * 2}px)`,
+              transition: "transform 0.1s ease-out",
+            }}
+            className="absolute left-[50%] inset-0 w-[50%] object-contain my-[6rem]"
+          >
+            <Image
+              src="/phone-img.webp"
+              alt="Foreground"
+              layout="fill"
+              objectFit="contain"
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+        <div className=" relative  w-[60%] h-full py-[10rem] pl-[5rem] z-10">
+          <div className="w-16 h-16 rounded-full bg-[linear-gradient(rgb(0,_237,_164)_0%,_rgb(106,_125,_241)_100%)] mx-auto flex items-center justify-center mb-2">
+            <span className="material-symbols-outlined text-3xl text-white">
+              mouse
+            </span>
+          </div>
+          <h1 className="bg-clip-text text-[4rem] font-bold w-full font-body text-transparent bg-gradient-to-r from-[#687bec] to-[#ff0e67]">
+            eLearning for all
+          </h1>
+          <ul className="mt-6 mb-14">
+            <li className="flex items-center justify-center gap-3 my-4 cursor-pointer hover:text-[#00ECA3] duration-300">
+              <span className="material-symbols-outlined text-4xl ">
+                public
+              </span>
+              <p className="text-3xl font-light tracking-wide">
+                Online Lessons
+              </p>
+            </li>
+            <li className="flex items-center justify-center gap-3 my-7 cursor-pointer hover:text-[#00ECA3] duration-300">
+              <span className="material-symbols-outlined text-4xl ">
+                menu_book
+              </span>
+              <p className="text-3xl font-light tracking-wide">Live Courses</p>
+            </li>
+            <li className="flex items-center justify-center gap-3 my-4 cursor-pointer hover:text-[#00ECA3] duration-300">
+              <span className="material-symbols-outlined text-4xl ">
+                brightness_empty
+              </span>
+              <p className="text-3xl font-light tracking-wide">
+                Certified Lessons
+              </p>
+            </li>
+          </ul>
+          <button className="flex items-center gap-3 mx-auto rounded-lg justify-center w-[13rem] p-4 rounded-tl-[2.3rem] bg-[linear-gradient(90deg,_rgb(0,_237,_164)_0%,_rgb(106,_125,_241)_100%)] text-white hover:shadow-xl hover:scale-105 duration-300">
+            <span className="material-symbols-outlined">visibility</span>
+            <p>View Courses</p>
+          </button>
+        </div>
+      </div>
+
+      <div className=" w-full h-[110vh] bg-[url('/bg1.jpg')] bg-cover bg-no-repeat bg-center object-contain  py-10">
         <div className=" w-[50%] h-full flex flex-col justify-center text-left items-center">
           <div className="pl-[8rem] pr-6">
             <h1 className="text-6xl font-body font-bold tracking-wide mb-4">
@@ -48,6 +165,7 @@ const HeroPage1 = () => {
               Get Access for Award Winning Online Courses{" "}
             </p>
           </div>
+
           <div className="my-4 flex items-center justify-center gap-10">
             <button className="flex items-center gap-3 rounded-lg justify-center w-[13rem] p-4 rounded-tl-[2.3rem] bg-[linear-gradient(90deg,_rgb(0,_237,_164)_0%,_rgb(106,_125,_241)_100%)] text-white hover:shadow-xl hover:scale-105 duration-300">
               <span className="material-symbols-outlined">search</span>
