9efc17c729f728edcdd075863e7e6f86002cc83f
Om Nigam <om2021064@akgec.ac.in>
2024-04-19 18:14:12 +0530

    starting login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,12 +8,14 @@ const Login = ({ isOpen, onClose }: any) => {
         <button onClick={onClose} className="absolute top-3 right-3">
           <span className="material-symbols-outlined ">close</span>
         </button>
-        <h2 className="font-body">
-          <span className="text-[2rem] font-medium">Welcome!</span>
-          <span className="">Login</span>
+        <h2 className="font-body flex items-center justify-center gap-2 mb-2">
+          <span className="text-[1.8rem] font-medium">Welcome!</span>
+          <span className="text-[1.8rem] font-bold bg-clip-text text-transparent bg-[linear-gradient(90deg,_rgb(0,_237,_164)_0%,_rgb(106,_125,_241)_100%)]">
+            Login
+          </span>
         </h2>
-        <p></p>
-        <form>
+        <p className="w-[5rem] h-[3px] bg-[#00eda4] mx-auto"></p>
+        <form className="border-2 border-red-500 p-2">
           <input type="email" placeholder="Email" required />
           <input type="password" placeholder="Password" required />
           <button type="submit">Log In</button>
