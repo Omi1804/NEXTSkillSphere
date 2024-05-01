61b561671654ce3aa06ab656904cc6564bc73449
Om Nigam <om2021064@akgec.ac.in>
2024-05-01 14:37:42 +0530

    modifying routes

--- src/pages/api/admin/course/index.ts
+++ src/pages/api/admin/course/index.ts
@@ -15,21 +15,38 @@ export default async function handler(
       await authenticateAdmin(req, res);
 
       const newCourse = req.body;
-      await Courses.create(newCourse);
+      // await Courses.create(newCourse);
+      await prisma.courses.create({
+        data: {
+          imageLink: newCourse.imageLink,
+          price: newCourse.price,
+          time: newCourse.time,
+          level: newCourse.level,
+          heading: newCourse.heading,
+          description: newCourse.description,
+          category: newCourse.category,
+          instructor: newCourse.instructor,
+        },
+      });
 
       res
         .status(200)
         .json({ message: "Course Created successfully", course: newCourse });
     } catch (error: any) {
+      console.log(error);
       return res.status(400).json({ message: error.message });
+    } finally {
+      await prisma.$disconnect();
     }
     //Get all the courses
   } else if (req.method === "GET") {
     try {
-      await connectToDb();
+      // await connectToDb();
       await authenticateAdmin(req, res);
 
-      const courses = await Courses.find({});
+      // const courses = await Courses.find({});
+      const courses = await prisma.courses.findMany();
+
       res.status(200).json(courses);
     } catch (error: any) {
       return res.status(400).json({ message: error.message });
