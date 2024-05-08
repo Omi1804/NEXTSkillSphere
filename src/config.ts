eba6b1c610e7ab9ea7fa26ad5a5b400a3a7d7875
Om Nigam <om2021064@akgec.ac.in>
2024-05-08 17:48:41 +0530

    Fix: bugs regarding models

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000/api";
-// export const BASE_URL = "https://next-skill-sphere.vercel.app/api";
+// export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "https://next-skill-sphere.vercel.app/api";
