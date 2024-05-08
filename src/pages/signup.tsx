db3a104319573582a129cdd04c317d94e2c157ed
Om Nigam <om2021064@akgec.ac.in>
2024-05-08 17:36:29 +0530

    Fix: bugs regarding models

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,210 +1,210 @@
-import React, { useState } from "react";
-import {
-  Typography,
-  AppBar,
-  Container,
-  TextField,
-  Button,
-  Box,
-  FormControlLabel,
-  Checkbox,
-  CssBaseline,
-  Snackbar,
-} from "@mui/material";
-import { useRouter } from "next/router";
-import { userState } from "@/store/atoms";
-import { useSetRecoilState } from "recoil";
-import axios from "axios";
-import { BASE_URL } from "@/config";
+// import React, { useState } from "react";
+// import {
+//   Typography,
+//   AppBar,
+//   Container,
+//   TextField,
+//   Button,
+//   Box,
+//   FormControlLabel,
+//   Checkbox,
+//   CssBaseline,
+//   Snackbar,
+// } from "@mui/material";
+// import { useRouter } from "next/router";
+// import { userState } from "@/store/atoms";
+// import { useSetRecoilState } from "recoil";
+// import axios from "axios";
+// import { BASE_URL } from "@/config";
 
-const Signup = () => {
-  //------------------------------VARIABLES------------------------------//
+// const Signup = () => {
+//   //------------------------------VARIABLES------------------------------//
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const setUserState = useSetRecoilState(userState);
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const setUserState = useSetRecoilState(userState);
 
-  const [openSnackbar, setOpenSnackbar] = useState(false); //for successfull signup
-  const [openSnackbar2, setOpenSnackbar2] = useState(false); //for already existing users
-  const router = useRouter();
+//   const [openSnackbar, setOpenSnackbar] = useState(false); //for successfull signup
+//   const [openSnackbar2, setOpenSnackbar2] = useState(false); //for already existing users
+//   const router = useRouter();
 
-  //------------------------------HANDLERS------------------------------//
+//   //------------------------------HANDLERS------------------------------//
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.target.name == "email"
-      ? setEmail(e.target.value)
-      : setPassword(e.target.value);
-  };
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     e.target.name == "email"
+//       ? setEmail(e.target.value)
+//       : setPassword(e.target.value);
+//   };
 
-  const handleSubmit = async () => {
-    try {
-      const response = await axios.post(`${BASE_URL}admin/signup`, {
-        email: email,
-        password: password,
-      });
+//   const handleSubmit = async () => {
+//     try {
+//       const response = await axios.post(`${BASE_URL}admin/signup`, {
+//         email: email,
+//         password: password,
+//       });
 
-      const responseData = await response.data;
-      if (responseData.message == "Admin created successfully") {
-        localStorage.setItem("token", responseData.token);
-        setOpenSnackbar(true);
-        setTimeout(() => {
-          router.push("/login");
-        }, 3000);
-      }
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        // Now TypeScript knows that error is an AxiosError
-        if (
-          error.response &&
-          error.response.status === 400 &&
-          error.response.data.message === "Admin already exists!"
-        ) {
-          setOpenSnackbar2(true);
-          setTimeout(() => {
-            router.push("/login");
-          }, 3000);
-        }
-      } else {
-        // Handle non-Axios errors here
-        console.error("An unexpected error occurred:", error);
-      }
-    }
-  };
+//       const responseData = await response.data;
+//       if (responseData.message == "Admin created successfully") {
+//         localStorage.setItem("token", responseData.token);
+//         setOpenSnackbar(true);
+//         setTimeout(() => {
+//           router.push("/login");
+//         }, 3000);
+//       }
+//     } catch (error: unknown) {
+//       if (axios.isAxiosError(error)) {
+//         // Now TypeScript knows that error is an AxiosError
+//         if (
+//           error.response &&
+//           error.response.status === 400 &&
+//           error.response.data.message === "Admin already exists!"
+//         ) {
+//           setOpenSnackbar2(true);
+//           setTimeout(() => {
+//             router.push("/login");
+//           }, 3000);
+//         }
+//       } else {
+//         // Handle non-Axios errors here
+//         console.error("An unexpected error occurred:", error);
+//       }
+//     }
+//   };
 
-  //------------------------------COMPONENTS------------------------------//
-  return (
-    <Container
-      maxWidth="sm"
-      component={Box}
-      boxShadow={3}
-      bgcolor="white"
-      mt={8}
-      p={3}
-      borderRadius={2}
-    >
-      <CssBaseline />
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={5000}
-        onClose={() => setOpenSnackbar(false)}
-        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-        ContentProps={{
-          style: {
-            backgroundColor: "#004d40",
-            color: "white",
-            borderRadius: "8px",
-          },
-        }}
-        message="Signed up successfully! Now login."
-        action={
-          <Button
-            size="small"
-            style={{ color: "#009688" }}
-            onClick={() => setOpenSnackbar(false)}
-          >
-            Close
-          </Button>
-        }
-      />
-      <Snackbar
-        open={openSnackbar2}
-        autoHideDuration={5000}
-        onClose={() => setOpenSnackbar(false)}
-        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-        ContentProps={{
-          style: {
-            backgroundColor: "#004d40",
-            color: "white",
-            borderRadius: "8px",
-          },
-        }}
-        message="You already have an account please Login."
-        action={
-          <Button
-            size="small"
-            style={{ color: "#009688" }}
-            onClick={() => setOpenSnackbar(false)}
-          >
-            Close
-          </Button>
-        }
-      />
+//   //------------------------------COMPONENTS------------------------------//
+//   return (
+//     <Container
+//       maxWidth="sm"
+//       component={Box}
+//       boxShadow={3}
+//       bgcolor="white"
+//       mt={8}
+//       p={3}
+//       borderRadius={2}
+//     >
+//       <CssBaseline />
+//       <Snackbar
+//         open={openSnackbar}
+//         autoHideDuration={5000}
+//         onClose={() => setOpenSnackbar(false)}
+//         anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+//         ContentProps={{
+//           style: {
+//             backgroundColor: "#004d40",
+//             color: "white",
+//             borderRadius: "8px",
+//           },
+//         }}
+//         message="Signed up successfully! Now login."
+//         action={
+//           <Button
+//             size="small"
+//             style={{ color: "#009688" }}
+//             onClick={() => setOpenSnackbar(false)}
+//           >
+//             Close
+//           </Button>
+//         }
+//       />
+//       <Snackbar
+//         open={openSnackbar2}
+//         autoHideDuration={5000}
+//         onClose={() => setOpenSnackbar(false)}
+//         anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+//         ContentProps={{
+//           style: {
+//             backgroundColor: "#004d40",
+//             color: "white",
+//             borderRadius: "8px",
+//           },
+//         }}
+//         message="You already have an account please Login."
+//         action={
+//           <Button
+//             size="small"
+//             style={{ color: "#009688" }}
+//             onClick={() => setOpenSnackbar(false)}
+//           >
+//             Close
+//           </Button>
+//         }
+//       />
 
-      <AppBar
-        position="relative"
-        elevation={0}
-        style={{
-          background: "linear-gradient(45deg, #009688, #004d40)",
-          marginBottom: "2rem",
-          borderRadius: "8px 8px 0 0",
-          boxShadow: "none",
-        }}
-      >
-        <Typography
-          variant="h5"
-          component="div"
-          sx={{ fontWeight: "bold", py: 1, px: 2 }}
-        >
-          Sign In
-        </Typography>
-      </AppBar>
+//       <AppBar
+//         position="relative"
+//         elevation={0}
+//         style={{
+//           background: "linear-gradient(45deg, #009688, #004d40)",
+//           marginBottom: "2rem",
+//           borderRadius: "8px 8px 0 0",
+//           boxShadow: "none",
+//         }}
+//       >
+//         <Typography
+//           variant="h5"
+//           component="div"
+//           sx={{ fontWeight: "bold", py: 1, px: 2 }}
+//         >
+//           Sign In
+//         </Typography>
+//       </AppBar>
 
-      <Box mb={3}>
-        <Typography variant="subtitle1" fontWeight="medium" mb={1}>
-          Enter your Email
-        </Typography>
-        <TextField
-          name="email"
-          id="email-input"
-          label="Email"
-          fullWidth
-          variant="outlined"
-          sx={{ mb: 2 }}
-          onChange={handleChange}
-        />
-      </Box>
+//       <Box mb={3}>
+//         <Typography variant="subtitle1" fontWeight="medium" mb={1}>
+//           Enter your Email
+//         </Typography>
+//         <TextField
+//           name="email"
+//           id="email-input"
+//           label="Email"
+//           fullWidth
+//           variant="outlined"
+//           sx={{ mb: 2 }}
+//           onChange={handleChange}
+//         />
+//       </Box>
 
-      <Box mb={3}>
-        <Typography variant="subtitle1" fontWeight="medium" mb={1}>
-          Enter your Password
-        </Typography>
-        <TextField
-          name="password"
-          id="password-input"
-          label="Password"
-          fullWidth
-          variant="outlined"
-          type="password"
-          onChange={handleChange}
-        />
-      </Box>
+//       <Box mb={3}>
+//         <Typography variant="subtitle1" fontWeight="medium" mb={1}>
+//           Enter your Password
+//         </Typography>
+//         <TextField
+//           name="password"
+//           id="password-input"
+//           label="Password"
+//           fullWidth
+//           variant="outlined"
+//           type="password"
+//           onChange={handleChange}
+//         />
+//       </Box>
 
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        mb={3}
-      >
-        <FormControlLabel
-          control={<Checkbox color="primary" />}
-          label="Remember Me"
-        />
-      </Box>
+//       <Box
+//         display="flex"
+//         justifyContent="space-between"
+//         alignItems="center"
+//         mb={3}
+//       >
+//         <FormControlLabel
+//           control={<Checkbox color="primary" />}
+//           label="Remember Me"
+//         />
+//       </Box>
 
-      <Button
-        variant="contained"
-        fullWidth
-        size="large"
-        style={{
-          backgroundColor: "#009688",
-          color: "white",
-        }}
-        onClick={() => handleSubmit()}
-      >
-        Sign In
-      </Button>
-    </Container>
-  );
-};
+//       <Button
+//         variant="contained"
+//         fullWidth
+//         size="large"
+//         style={{
+//           backgroundColor: "#009688",
+//           color: "white",
+//         }}
+//         onClick={() => handleSubmit()}
+//       >
+//         Sign In
+//       </Button>
+//     </Container>
+//   );
+// };
 
-export default Signup;
+// export default Signup;
