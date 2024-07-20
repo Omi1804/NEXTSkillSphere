2b6893f3242c7e584b7524b363ff162b3f352fd7
Om Nigam <om2021064@akgec.ac.in>
2024-07-21 02:04:18 +0530

    adding OG tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,18 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
         />
+        <meta property="og:title" content="Next-Skill Sphere" />
+        <meta
+          property="og:description"
+          content="NextSkill Sphere a Robust Education and Online Learning Website featuring Online Teaching, Online Courses, Learning materials, elearning services."
+        />
+
+        <meta
+          property="og:url"
+          content="https://next-skill-sphere.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Next Skill Sphere" />
       </Head>
       <body>
         <Main />
