5cce5d490d16473235cb5223d6e1a78d4c145941
Om Nigam <devomini18@gmail.com>
2026-02-16 01:03:20 +0530

    Add organization schema for structured data in MetaTags component

--- src/app/meta.tsx
+++ src/app/meta.tsx
@@ -1,3 +1,16 @@
+const SITE_URL = "https://next-skill-sphere.vercel.app/";
+
+const organizationSchema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  name: "Skill Sphere",
+  url: SITE_URL,
+  description:
+    "Skill Sphere is a course platform to learn modern tech skills with practical projects.",
+
+  logo: "https://next-skill-sphere.vercel.app/favicon_io/android-chrome-192x192.png",
+};
+
 const MetaTags = () => {
   return (
     <head>
@@ -65,6 +78,14 @@ const MetaTags = () => {
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+      />
+
+      {/* scripts */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({ organizationSchema }),
+        }}
       />
     </head>
   );
