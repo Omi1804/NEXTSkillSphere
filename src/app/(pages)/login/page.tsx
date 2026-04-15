261b47f000424421b647505d1e252dc8f5b2f314
Om Nigam <devomini18@gmail.com>
2026-04-15 20:40:00 +0530

    fix

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,13 +1,13 @@
 import CommonHero from "@/components/CommonHero";
 import LoginPageClient from "@/components/Login/LoginPageClient";
+import { Suspense } from "react";
 
 const LoginPage = () => {
-  // seperate login page
   return (
-    <>
+    <Suspense>
       <CommonHero Image="/breadcrumb-whyus.png" heroHeading="Login" subHeading="LOGIN" />
       <LoginPageClient />
-    </>
+    </Suspense>
   );
 };
 
