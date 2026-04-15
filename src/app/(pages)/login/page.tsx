f2f29bbd14f9973049745c5c41a9c642c599056b
Om Nigam <devomini18@gmail.com>
2026-04-15 17:29:06 +0530

    fix

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -2,6 +2,7 @@ import CommonHero from "@/components/CommonHero";
 import LoginPageClient from "@/components/Login/LoginPageClient";
 
 const LoginPage = () => {
+  // seperate login page
   return (
     <>
       <CommonHero Image="/breadcrumb-whyus.png" heroHeading="Login" subHeading="LOGIN" />
